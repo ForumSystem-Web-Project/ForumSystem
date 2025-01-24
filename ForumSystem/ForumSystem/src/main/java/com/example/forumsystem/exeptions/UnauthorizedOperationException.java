e8efa4c06b3b7a839107db658cf64432aed3a676
Chavdar Tsvetkov <chavdartsvetkov@outlook.com>
2025-01-24 14:08:25 +0200

    Delete ForumSystem directory

--- ForumSystem/ForumSystem/src/main/java/com/example/forumsystem/exeptions/UnauthorizedOperationException.java
+++
@@ -1,8 +0,0 @@
-package com.example.forumsystem.exeptions;
-
-public class UnauthorizedOperationException extends RuntimeException{
-
-    public UnauthorizedOperationException(String message) {
-        super(message);
-    }
-}
